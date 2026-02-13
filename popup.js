d0f8ba189d831fb3e6931f9695dedf7b625c63fb
aleksey <aleksey99@live.com>
2026-02-12 20:56:47 -0500

    add dark mode toggle

--- popup.js
+++ popup.js
@@ -4,6 +4,7 @@ chrome.storage.local.get(
     totalCount: 0,
     workHoursCount: 0,
     nonWorkHoursCount: 0,
+    darkMode: false,
   },
   (data) => {
     document.getElementById("dailyCount").textContent = data.dailyCount;
@@ -11,8 +12,26 @@ chrome.storage.local.get(
     document.getElementById("workHoursCount").textContent = data.workHoursCount;
     document.getElementById("nonWorkHoursCount").textContent =
       data.nonWorkHoursCount;
+
+    const btn = document.getElementById("themeToggle");
+
+    if (data.darkMode) {
+      document.body.classList.add("dark");
+      btn.textContent = "☀️";
+    } else {
+      btn.textContent = "🌙";
+    }
   },
 );
+
+document.getElementById("themeToggle").addEventListener("click", () => {
+  const isDark = document.body.classList.toggle("dark");
+
+  const btn = document.getElementById("themeToggle");
+  btn.textContent = isDark ? "☀️" : "🌙";
+
+  chrome.storage.local.set({ darkMode: isDark });
+});
 
 document.getElementById("saveBtn").addEventListener("click", () => {
   const dashboard = document.getElementById("dashboard");
